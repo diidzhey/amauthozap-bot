c566c8f89317db51138fe5b35227fc201382001d
Julia <dii.dzhey@gmail.com>
2026-06-11 15:29:53 +0500

    Исправление ошибки

--- diplom.py
+++ diplom.py
@@ -19,7 +19,7 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 # Настройки OpenRouter API
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
-MODEL = "google/gemma-4-26b-a4b-it:free"
+MODEL = "qwen/qwen3-next-80b-a3b-instruct:free"
 
 if not OPENROUTER_API_KEY:
     raise ValueError("❌ Вставьте API-ключ OpenRouter!")
